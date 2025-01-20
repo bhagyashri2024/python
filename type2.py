6f14fbcbd21e772856cfd9c8263346fb8c1d583e
unknown <bhagyashribhatkar32@gmail.com>
2025-01-20 12:05:47 +0530

    first commit

---
+++ type2.py
@@ -0,0 +1,17 @@
+#python code to demonstrate type conversion
+#using ord(),hex(),oct()
+#intializing integer
+s='4'
+#printing character converting to integer
+c=ord(s)
+print ("afterconverting character to integer:",end="")
+print(c)
+#printinginteger converting to hexadecimal string
+c=hex(56)
+print("after converting 56 to hexadecimal string:",end="")
+print (c)
+#printing integer convertingto octal string
+c=oct (56)
+print("After converting 56 to octal string:",end="")
+print( c)
+
