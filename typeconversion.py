6a76fdecf36198757dff6efb2d484035fd91d65d
unknown <bhagyashribhatkar32@gmail.com>
2025-01-20 11:22:01 +0530

    first commit

---
+++ typeconversion.py
@@ -0,0 +1,12 @@
+#python code to demonstrate type conversion
+#using int(),float()
+#intializing string
+s="10010"
+#printing string converting to int base2
+c=int(s,2)
+print("After converting to integer base2 :",end="")
+print (c)
+#printing string converting to float
+e=float(s)
+print("after convertingtp float :",end="")
+print(e)
